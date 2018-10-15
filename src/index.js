d97062249de36cee3197bba3d285ffced01e4a30
Worapong Malaiwong <rpg2xjob.doiinn@gmail.com>
2018-10-16 03:17:20 +0700

    disable service worker temporary

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import rootReducer from './reducers'
 import { Provider } from 'react-redux'
 import { createBrowserHistory } from 'history'
@@ -29,4 +29,4 @@ const render = () => {
 }
 
 render()
-registerServiceWorker()
+// registerServiceWorker()
